6043942c89f3aa7bf680e5650c917b8c1de59de9
Nathan Donolli <ndonolli@gmail.com>
2017-05-09 10:37:39 -0500

    add publish script

---
+++ publish.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Script to automatically publish the site to the github pages repo
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )" # gets directory of this bash script
+if [[ $PWD !=  $DIR ]]
+then
+  echo "Error: Current working directory is not the main website repo directory"
+  exit 1
+fi
+
+hugo -t hemingway
+cd "${PWD}/public"
+git add .
+git commit -m "site build $(date)"
+git push origin master
